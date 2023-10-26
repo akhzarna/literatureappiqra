1b03273b7b12a3468b4dd5513466e2db5dcd42ce
Saim Hafeez <saimhafeez2020@gmail.com>
2023-10-26 13:31:08 +0500

    FA20-BSE-011 Section B Saim Hafeez

--- component/ApiComponent.js
+++ component/ApiComponent.js
@@ -1,27 +1,21 @@
 import axios from "axios";
-import React, { useEffect } from "react";
-import { Text, View } from "react-native";
 
-const ApiComponent = () => {
-	useEffect(() => {
-		// Define the API endpoint
-		const apiUrl = "http://139.59.177.72/api/books?page=1"; // Example API
-		// Make an API request
-		axios
-			.get(apiUrl)
-			.then((response) => {
-				console.log("API Response:", response.data);
-			})
-			.catch((error) => {
-				console.error("API Error:", error);
-			});
-	}, []);
+export const getApiData = () => {
+	return new Promise((res, rej) => {
+		try {
+			const apiUrl = "http://139.59.177.72/api/books?page=1";
+			axios
+				.get(apiUrl)
+				.then((response) => {
 
-	return (
-		<View>
-			<Text>Check the console for API response</Text>
-		</View>
-	);
-};
-
-export default ApiComponent;
+					res(response.data)
+				})
+				.catch((error) => {
+					console.error("API Error:", error);
+					rej(error)
+				});
+		} catch (error) {
+			rej(error)
+		}
+	})
+}
---
+++ src/BookChapters.js
@@ -0,0 +1,40 @@
+import { useRoute } from "@react-navigation/native"
+import { useEffect, useState } from "react"
+import { FlatList, Text, View } from "react-native";
+
+function BookChapters() {
+
+  const route = useRoute()
+
+  const [chapters, setChapters] = useState([])
+
+  useEffect(() => {
+
+    setChapters(route.params.chapters);
+
+  }, [])
+
+  return (
+    <View>
+      <FlatList
+        data={chapters}
+        contentContainerStyle={{
+          gap: 10,
+          padding: 5
+        }}
+        renderItem={({ item, index }) => (
+          <Text
+            style={{
+              padding: 10,
+              backgroundColor: '#e2e3e5',
+              borderRadius: 10
+            }}
+          >{item}</Text>
+        )}
+      />
+    </View>
+  )
+
+}
+
+export default BookChapters
